8934cc2e24a8f23924eec4b585c534946624f0c8
Sergio San Pedro Álvarez <sergiospalvarez@hotmail.com>
2025-06-23 14:46:37 +0000

    sin terminar

--- src/app.js
+++ src/app.js
@@ -5,20 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function () {
-  //write your code here
-  console.log("Hello Rigo from the console!");
 
-  let pronoun = ['the', 'our'];
-  let adj = ['great', 'big'];
-  let noun = ['jogger', 'racoon'];
-  let dominios = ['.com', '.net', '.info', '.org', '.biz', '.edu']
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let dominios = ['.com', '.net', '.info', '.org', '.biz', '.edu']
 
-  const domain = pronoun.flatMap(pronouns => adj.flatMap(adje => noun.flatMap(noum => dominios.map(dom =>
-    pronouns + adje + noum + dom))))
+const domain = pronoun.flatMap(pronouns => adj.flatMap(adje => noun.flatMap(noum => dominios.map(dom =>
+  pronouns + adje + noum + dom))))
 
-  console.log(domain);
+
+console.log(domain);
 
 
 
-};
+// conectar boton-html
+
+let botonComercio = document.getElementById('comercios')
+let titulo = document.getElementById('listaDominios')
+
+// Funcion para filtrar acabados en .com
+
+botonComercio.addEventListener('click', function() {
+
+  const comercios = domain.filter(dominio => {
+    return dominio.endsWith('.com')
+  })
+
+  titulo.innerHTML = comercios.join('<br>')
+
+})
