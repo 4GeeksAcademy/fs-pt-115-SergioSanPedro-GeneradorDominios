1274e1bfd26b8a563a829301d00beda4be9c2d8e
Sergio San Pedro Álvarez <sergiospalvarez@hotmail.com>
2025-06-23 11:14:18 +0000

    previo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let dominios = ['.com', '.net', '.info', '.org', '.biz', '.edu']
+
+  const domain = pronoun.flatMap(pronouns => adj.flatMap(adje => noun.flatMap(noum => dominios.map(dom =>
+    pronouns + adje + noum + dom))))
+
+  console.log(domain);
+
+
+
 };
