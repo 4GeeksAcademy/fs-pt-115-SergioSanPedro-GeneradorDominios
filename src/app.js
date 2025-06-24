610142f27fa40a203a9b6c108f2f2c4ccb1b8c9e
Sergio San Pedro Álvarez <sergiospalvarez@hotmail.com>
2025-06-24 14:53:37 +0000

    detalles

--- src/app.js
+++ src/app.js
@@ -19,12 +19,20 @@ console.log(domain);
 
 
 
-// conectar boton-html
+// conectar botones-html
 
 let botonComercio = document.getElementById('comercios')
+let botonInformacion = document.getElementById('informacion')
+let botonRedes = document.getElementById('redes')
+let botonOrganizaciones = document.getElementById('organizaciones')
+let botonNegocios = document.getElementById('negocios')
+let botonEducacion = document.getElementById('educacion')
+
 let titulo = document.getElementById('listaDominios')
 
-// Funcion para filtrar acabados en .com
+
+
+// Funcion para filtrar segun dominios
 
 botonComercio.addEventListener('click', function() {
 
@@ -32,6 +40,56 @@ botonComercio.addEventListener('click', function() {
     return dominio.endsWith('.com')
   })
 
-  titulo.innerHTML = comercios.join('<br>')
+  titulo.innerHTML = 'Los dominios en ese campo son: ' + '<br>' + comercios.join('<br>');
+
+})
+
+botonInformacion.addEventListener('click', function() {
+
+  const inform = domain.filter(dominio => {
+    return dominio.endsWith('.info')
+  })
+
+  titulo.innerHTML= 'Los dominios en ese campo son: ' + '<br>' + inform.join('<br>')
+
+})
+
+botonRedes.addEventListener('click', function() {
+
+  const redes = domain.filter(red => {
+    return red.endsWith('.net')
+  })
+
+  titulo.innerHTML = 'Los dominios en ese campo son: ' + '<br>' + redes.join('<br>')
+
+})
+
+botonOrganizaciones.addEventListener('click', function() {
+
+  const organizaciones = domain.filter(org => {
+    return org.endsWith('.org')
+  })
+
+  titulo.innerHTML = 'Los dominios en ese campo son: ' + '<br>' + organizaciones.join('<br>')
+
+})
+
+botonNegocios.addEventListener('click', function() {
+
+  const negocios = domain.filter(neg => {
+    return neg.endsWith('.net')
+  })
+
+  titulo.innerHTML = 'Los dominios en ese campo son: ' + '<br>' + negocios.join('<br>')
+
+})
+
+botonEducacion.addEventListener('click', function() {
+
+  const educacion = domain.filter(edu => {
+    return edu.endsWith('.edu')
+  })
+
+  titulo.innerHTML = 'Los dominios en ese campo son: ' + '<br>' + educacion.join('<br>')
 
 })
